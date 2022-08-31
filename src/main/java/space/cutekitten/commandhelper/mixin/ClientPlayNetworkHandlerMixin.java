7ce0c48905a85407ae41ba5cc7ba815f1a383542
Some Kitten <blueredkitten@gmail.com>
2022-08-30 23:35:24 -0400

    Fixed integrated server not updating scores properly

--- src/main/java/space/cutekitten/commandhelper/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/space/cutekitten/commandhelper/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,9 +1,9 @@
 package space.cutekitten.commandhelper.mixin;
 
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.packet.s2c.play.ScoreboardObjectiveUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ScoreboardPlayerUpdateS2CPacket;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,20 +15,22 @@ import space.cutekitten.commandhelper.client.ScoreboardRenderer;
 public class ClientPlayNetworkHandlerMixin {
     @Inject(method = "onScoreboardObjectiveUpdate", at = @At("RETURN"))
     private void onScoreboardObjectiveUpdate(ScoreboardObjectiveUpdateS2CPacket packet, CallbackInfo ci) {
-        if (ClientDB.client.isIntegratedServerRunning()) return;
-
-        ClientWorld world = ClientDB.client.world;
+        World world = ClientDB.client.world;
         if (world == null) return;
+
+        if (ClientDB.client.isIntegratedServerRunning())
+            world = ClientDB.client.getServer().getWorld(world.getRegistryKey());
 
         ScoreboardRenderer.updateScoreboard(world.getScoreboard());
     }
 
     @Inject(method = "onScoreboardPlayerUpdate", at = @At("RETURN"))
     private void onScoreboardPlayerUpdate(ScoreboardPlayerUpdateS2CPacket packet, CallbackInfo ci) {
-        if (ClientDB.client.isIntegratedServerRunning()) return;
-
-        ClientWorld world = ClientDB.client.world;
+        World world = ClientDB.client.world;
         if (world == null) return;
+
+        if (ClientDB.client.isIntegratedServerRunning())
+            world = ClientDB.client.getServer().getWorld(world.getRegistryKey());
 
         ScoreboardRenderer.updateScoreboard(world.getScoreboard());
     }
